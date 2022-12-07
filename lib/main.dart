def148aed56e3b0a893e57bb01e638f843a2f2c1
zakieass <zakisetyawan2@gmail.com>
2022-12-07 18:10:42 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/makanterusapp/generatedhalamanawalwidget/GeneratedHalamanawalWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedpilihanmenuadminwidget/GeneratedPilihanmenuadminWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedadminwidget/GeneratedAdminWidget.dart';
+import 'package:flutterapp/makanterusapp/generateduserwidget/GeneratedUserWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedguestwidget/GeneratedGuestWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekategoriwidget1/GeneratedManagekategoriWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedisikategoriwidget/GeneratedIsikategoriWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageprodukterataswidget1/GeneratedManageprodukteratasWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagediskonwidget1/GeneratedManagediskonWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekategoriwidget2/GeneratedManagekategoriWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageprodukterataswidget2/GeneratedManageprodukteratasWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagediskonwidget2/GeneratedManagediskonWidget2.dart';
+import 'package:flutterapp/makanterusapp/generateddeskripsiwidget/GeneratedDeskripsiWidget.dart';
+import 'package:flutterapp/makanterusapp/generateduploadfotowidget/GeneratedUploadfotoWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekomentarwidget/GeneratedManagekomentarWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekomentarwidget1/GeneratedManagekomentarWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedbahasawidget/GeneratedBahasaWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedkomentarlikewidget/GeneratedKomentarLikeWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageuserwidget1/GeneratedManageuserWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedprofilwidget/GeneratedProfilWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedcruprofillogoutwidget/GeneratedCRUprofilLogoutWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedhalamanawalwidget1/GeneratedHalamanawalWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedkategoriwidget3/GeneratedKategoriWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedisikategoriwidget1/GeneratedIsikategoriWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedprofilwidget1/GeneratedProfilWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedcruprofillogoutwidget1/GeneratedCRUprofilLogoutWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedbahasawidget1/GeneratedBahasaWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedhalamanawalwidget2/GeneratedHalamanawalWidget2.dart';
+import 'package:flutterapp/makanterusapp/generateddeskripsiwidget2/GeneratedDeskripsiWidget2.dart';
+import 'package:flutterapp/makanterusapp/generateddeskripsiwidget3/GeneratedDeskripsiWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedkomentarlikewidget1/GeneratedKomentarLikeWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedkategoriwidget5/GeneratedKategoriWidget5.dart';
+import 'package:flutterapp/makanterusapp/generatedisikategoriwidget2/GeneratedIsikategoriWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedemailidwidget4/GeneratedEmailidWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedpwwidget2/GeneratedPWWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatednotifikasiwidget/GeneratedNotifikasiWidget.dart';
+import 'package:flutterapp/makanterusapp/generatednotifikasiwidget2/GeneratedNotifikasiWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedsukawidget/GeneratedSukaWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedsukawidget2/GeneratedSukaWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatednamapenggunawidget1/GeneratedNamapenggunaWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedpilihanmenuadminwidget1/GeneratedPilihanmenuadminWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedtampilanawalwidget/GeneratedTampilanawalWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedbahasawidget3/GeneratedBahasaWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedbahasawidget4/GeneratedBahasaWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedbahasawidget5/GeneratedBahasaWidget5.dart';
+import 'package:flutterapp/makanterusapp/generatedtampilanawalwidget1/GeneratedTampilanawalWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedtampilanawalwidget2/GeneratedTampilanawalWidget2.dart';
+import 'package:flutterapp/makanterusapp/generateduploadfotowidget3/GeneratedUploadfotoWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekomentarwidget2/GeneratedManagekomentarWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedkomentarlikewidget2/GeneratedKomentarLikeWidget2.dart';
+import 'package:flutterapp/makanterusapp/generateddeskripsiwidget4/GeneratedDeskripsiWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedsignupwidget1/GeneratedSignupWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedhalamanloginwidget/GeneratedHalamanLoginWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedhalamanloginwidget1/GeneratedHalamanLoginWidget1.dart';
+import 'package:flutterapp/makanterusapp/generateddeleteartikelwidget/GeneratedDeleteartikelWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekategoriwidget3/GeneratedManagekategoriWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageprodukterataswidget3/GeneratedManageprodukteratasWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagediskonwidget3/GeneratedManagediskonWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedkategoriwidget16/GeneratedKategoriWidget16.dart';
+import 'package:flutterapp/makanterusapp/generatedisikategoriwidget3/GeneratedIsikategoriWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagekategoriwidget4/GeneratedManagekategoriWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageprodukterataswidget4/GeneratedManageprodukteratasWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedmanagediskonwidget4/GeneratedManagediskonWidget4.dart';
+import 'package:flutterapp/makanterusapp/generatedprofilwidget2/GeneratedProfilWidget2.dart';
+import 'package:flutterapp/makanterusapp/generatedprofilwidget3/GeneratedProfilWidget3.dart';
+import 'package:flutterapp/makanterusapp/generatedcruprofilwidget/GeneratedCRUprofilWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedcruprofilwidget/GeneratedCRUProfilWidget.dart';
+import 'package:flutterapp/makanterusapp/generatedmanageuserwidget4/GeneratedManageuserWidget4.dart';
+import 'package:flutterapp/makanterusapp/generateduserwidget1/GeneratedUserWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedadminwidget1/GeneratedAdminWidget1.dart';
+import 'package:flutterapp/makanterusapp/generatedguestwidget1/GeneratedGuestWidget1.dart';
+
+void main() {
+  runApp(MakanTerusApp());
+}
+
+class MakanTerusApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedHalamanawalWidget',
+      routes: {
+        '/GeneratedHalamanawalWidget': (context) =>
+            GeneratedHalamanawalWidget(),
+        '/GeneratedPilihanmenuadminWidget': (context) =>
+            GeneratedPilihanmenuadminWidget(),
+        '/GeneratedAdminWidget': (context) => GeneratedAdminWidget(),
+        '/GeneratedUserWidget': (context) => GeneratedUserWidget(),
+        '/GeneratedGuestWidget': (context) => GeneratedGuestWidget(),
+        '/GeneratedManagekategoriWidget1': (context) =>
+            GeneratedManagekategoriWidget1(),
+        '/GeneratedIsikategoriWidget': (context) =>
+            GeneratedIsikategoriWidget(),
+        '/GeneratedManageprodukteratasWidget1': (context) =>
+            GeneratedManageprodukteratasWidget1(),
+        '/GeneratedManagediskonWidget1': (context) =>
+            GeneratedManagediskonWidget1(),
+        '/GeneratedManagekategoriWidget2': (context) =>
+            GeneratedManagekategoriWidget2(),
+        '/GeneratedManageprodukteratasWidget2': (context) =>
+            GeneratedManageprodukteratasWidget2(),
+        '/GeneratedManagediskonWidget2': (context) =>
+            GeneratedManagediskonWidget2(),
+        '/GeneratedDeskripsiWidget': (context) => GeneratedDeskripsiWidget(),
+        '/GeneratedUploadfotoWidget': (context) => GeneratedUploadfotoWidget(),
+        '/GeneratedManagekomentarWidget': (context) =>
+            GeneratedManagekomentarWidget(),
+        '/GeneratedManagekomentarWidget1': (context) =>
+            GeneratedManagekomentarWidget1(),
+        '/GeneratedBahasaWidget': (context) => GeneratedBahasaWidget(),
+        '/GeneratedKomentarLikeWidget': (context) =>
+            GeneratedKomentarLikeWidget(),
+        '/GeneratedManageuserWidget1': (context) =>
+            GeneratedManageuserWidget1(),
+        '/GeneratedProfilWidget': (context) => GeneratedProfilWidget(),
+        '/GeneratedCRUprofilLogoutWidget': (context) =>
+            GeneratedCRUprofilLogoutWidget(),
+        '/GeneratedHalamanawalWidget1': (context) =>
+            GeneratedHalamanawalWidget1(),
+        '/GeneratedKategoriWidget3': (context) => GeneratedKategoriWidget3(),
+        '/GeneratedIsikategoriWidget1': (context) =>
+            GeneratedIsikategoriWidget1(),
+        '/GeneratedProfilWidget1': (context) => GeneratedProfilWidget1(),
+        '/GeneratedCRUprofilLogoutWidget1': (context) =>
+            GeneratedCRUprofilLogoutWidget1(),
+        '/GeneratedBahasaWidget1': (context) => GeneratedBahasaWidget1(),
+        '/GeneratedHalamanawalWidget2': (context) =>
+            GeneratedHalamanawalWidget2(),
+        '/GeneratedDeskripsiWidget2': (context) => GeneratedDeskripsiWidget2(),
+        '/GeneratedDeskripsiWidget3': (context) => GeneratedDeskripsiWidget3(),
+        '/GeneratedKomentarLikeWidget1': (context) =>
+            GeneratedKomentarLikeWidget1(),
+        '/GeneratedKategoriWidget5': (context) => GeneratedKategoriWidget5(),
+        '/GeneratedIsikategoriWidget2': (context) =>
+            GeneratedIsikategoriWidget2(),
+        '/GeneratedEmailidWidget4': (context) => GeneratedEmailidWidget4(),
+        '/GeneratedPWWidget2': (context) => GeneratedPWWidget2(),
+        '/GeneratedNotifikasiWidget': (context) => GeneratedNotifikasiWidget(),
+        '/GeneratedNotifikasiWidget2': (context) =>
+            GeneratedNotifikasiWidget2(),
+        '/GeneratedSukaWidget': (context) => GeneratedSukaWidget(),
+        '/GeneratedSukaWidget2': (context) => GeneratedSukaWidget2(),
+        '/GeneratedNamapenggunaWidget1': (context) =>
+            GeneratedNamapenggunaWidget1(),
+        '/GeneratedPilihanmenuadminWidget1': (context) =>
+            GeneratedPilihanmenuadminWidget1(),
+        '/GeneratedTampilanawalWidget': (context) =>
+            GeneratedTampilanawalWidget(),
+        '/GeneratedBahasaWidget3': (context) => GeneratedBahasaWidget3(),
+        '/GeneratedBahasaWidget4': (context) => GeneratedBahasaWidget4(),
+        '/GeneratedBahasaWidget5': (context) => GeneratedBahasaWidget5(),
+        '/GeneratedTampilanawalWidget1': (context) =>
+            GeneratedTampilanawalWidget1(),
+        '/GeneratedTampilanawalWidget2': (context) =>
+            GeneratedTampilanawalWidget2(),
+        '/GeneratedUploadfotoWidget3': (context) =>
+            GeneratedUploadfotoWidget3(),
+        '/GeneratedManagekomentarWidget2': (context) =>
+            GeneratedManagekomentarWidget2(),
+        '/GeneratedKomentarLikeWidget2': (context) =>
+            GeneratedKomentarLikeWidget2(),
+        '/GeneratedDeskripsiWidget4': (context) => GeneratedDeskripsiWidget4(),
+        '/GeneratedSignupWidget1': (context) => GeneratedSignupWidget1(),
+        '/GeneratedHalamanLoginWidget': (context) =>
+            GeneratedHalamanLoginWidget(),
+        '/GeneratedHalamanLoginWidget1': (context) =>
+            GeneratedHalamanLoginWidget1(),
+        '/GeneratedDeleteartikelWidget': (context) =>
+            GeneratedDeleteartikelWidget(),
+        '/GeneratedManagekategoriWidget3': (context) =>
+            GeneratedManagekategoriWidget3(),
+        '/GeneratedManageprodukteratasWidget3': (context) =>
+            GeneratedManageprodukteratasWidget3(),
+        '/GeneratedManagediskonWidget3': (context) =>
+            GeneratedManagediskonWidget3(),
+        '/GeneratedKategoriWidget16': (context) => GeneratedKategoriWidget16(),
+        '/GeneratedIsikategoriWidget3': (context) =>
+            GeneratedIsikategoriWidget3(),
+        '/GeneratedManagekategoriWidget4': (context) =>
+            GeneratedManagekategoriWidget4(),
+        '/GeneratedManageprodukteratasWidget4': (context) =>
+            GeneratedManageprodukteratasWidget4(),
+        '/GeneratedManagediskonWidget4': (context) =>
+            GeneratedManagediskonWidget4(),
+        '/GeneratedProfilWidget2': (context) => GeneratedProfilWidget2(),
+        '/GeneratedProfilWidget3': (context) => GeneratedProfilWidget3(),
+        '/GeneratedCRUprofilWidget': (context) => GeneratedCRUprofilWidget(),
+        '/GeneratedCRUProfilWidget': (context) => GeneratedCRUProfilWidget(),
+        '/GeneratedManageuserWidget4': (context) =>
+            GeneratedManageuserWidget4(),
+        '/GeneratedUserWidget1': (context) => GeneratedUserWidget1(),
+        '/GeneratedAdminWidget1': (context) => GeneratedAdminWidget1(),
+        '/GeneratedGuestWidget1': (context) => GeneratedGuestWidget1(),
+      },
+    );
+  }
+}
